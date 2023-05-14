bb0a5e0a25fd524350726e4a6aefeedbaa2f5286
rniman <105342620+rniman@users.noreply.github.com>
2023-05-15 02:28:25 +0900

    따라하기 06-01-06 수정

--- LabProject/Shaders.hlsl
+++ LabProject/Shaders.hlsl
@@ -38,7 +38,7 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) + 0.5f;
+    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f;
     f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
     float fLength = length(f2NDC);
     float fMin = 0.3f, fMax = 0.2f;
