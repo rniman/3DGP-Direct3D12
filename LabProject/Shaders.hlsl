0e0c1b61204ff3cf8071f4962428cfa6398813ea
rniman <105342620+rniman@users.noreply.github.com>
2023-05-15 02:21:52 +0900

    따라하기 06-01-03

--- LabProject/Shaders.hlsl
+++ LabProject/Shaders.hlsl
@@ -32,14 +32,17 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 }
 
 
-#define FRAME_BUFFER_WIDTH 640.0f
-#define FRAME_BUFFER_HEIGHT 480.0f
+#define FRAME_BUFFER_WIDTH 800.0f
+#define FRAME_BUFFER_HEIGHT 600.0f
 //픽셀 쉐이더를 다음과 같이 정의하여 실행해 보면 다음과 같은 결과를 볼 수 있다.
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    cColor.r = input.x /  FRAME_BUFFER_WIDTH;
-    cColor.g = input.y / FRAME_BUFFER_HEIGHT;
+    //cColor.rgb = distance(float2(0.0f, 0.0f), input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT));
+    //cColor.rgb = length(input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT)); 
+    //cColor.rgb = distance(float2(0.0f, 0.0f), (input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT) -0.5f)); // (0, 1) : (-0.5, 0.5)
+    //cColor.rgb = length(input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT) - 0.5f);
+    cColor.rgb = distance(float2(0.5f, 0.5f), input.xy / float2(FRAME_BUFFER_WIDTH, FRAME_BUFFER_HEIGHT));
     
     return (cColor);
 }
