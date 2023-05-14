0f24c8f44494bf21a8e6570821fd1c50f4e0e89a
rniman <105342620+rniman@users.noreply.github.com>
2023-05-15 02:33:39 +0900

    따라하기 06-01-11

--- LabProject/Shaders.hlsl
+++ LabProject/Shaders.hlsl
@@ -35,21 +35,11 @@ float4 VSMain(uint nVertexID : SV_VertexID) : SV_POSITION
 #define FRAME_BUFFER_WIDTH 800.0f
 #define FRAME_BUFFER_HEIGHT 600.0f
 //픽셀 쉐이더를 다음과 같이 정의하여 실행해 보면 다음과 같은 결과를 볼 수 있다.
-float RegularPolygon(float2 f2NDC, float fSides, float fRadius)
-{
-    float fAngle = atan(f2NDC.y / f2NDC.x);
-    float fSlices = (2.0f * 3.14159f) / fSides;
-    float fShape = step(cos(floor((fAngle / fSlices) + 0.5f) * fSlices - fAngle) * length(f2NDC), fRadius);
-    return (fShape);
-}
-
 float4 PSMain(float4 input : SV_POSITION) : SV_TARGET
 {
     float4 cColor = float4(0.0f, 0.0f, 0.0f, 1.0f);
-    float2 f2NDC = float2(input.x / FRAME_BUFFER_WIDTH, input.y / FRAME_BUFFER_HEIGHT) - 0.5f; // (0, 1) : (-0.5, 0.5)
-    f2NDC.x *= (FRAME_BUFFER_WIDTH / FRAME_BUFFER_HEIGHT);
-
-    cColor.b = RegularPolygon(f2NDC - float2(-0.3f, -0.1f), 8.0f, 0.2f); //4, 6, 8, ... 
-    cColor.r = RegularPolygon(f2NDC - float2(+0.3f, +0.2f), 4.0f, 0.15f);
+    float x = abs(frac((input.x * 10.0f) / FRAME_BUFFER_HEIGHT) - 0.5f);
+    float y = abs(frac((input.y * 10.0f) / FRAME_BUFFER_HEIGHT) - 0.5f);
+    cColor.r = ((x <= 0.0125f) || (y <= 0.0125f)) ? 1.0f : 0.0f;
     return (cColor);
 }
