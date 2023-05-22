34126fdd92469137a32a62728d11fe714bedc8ae
rniman <105342620+rniman@users.noreply.github.com>
2023-05-23 01:28:47 +0900

    따라하기 09(직육면체)

--- LabProject/stdafx.h
+++ LabProject/stdafx.h
@@ -51,6 +51,10 @@ using Microsoft::WRL::ComPtr;
 constexpr UINT FRAME_BUFFER_WIDTH{ 800 };
 constexpr UINT FRAME_BUFFER_HEIGHT{ 600 };
 
+/*정점의 색상을 무작위로(Random) 설정하기 위해 사용한다. 각 정점의 색상은 난수(Random Number)를 생성하여
+지정한다.*/
+#define RANDOM_COLOR  XMFLOAT4(rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX), rand() / float(RAND_MAX))
+
 extern ID3D12Resource* CreateBufferResource(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, void* pData, UINT nBytes, D3D12_HEAP_TYPE d3dHeapType = D3D12_HEAP_TYPE_UPLOAD,
 	D3D12_RESOURCE_STATES d3dResourceStates = D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, ID3D12Resource** ppd3dUploadBuffer = nullptr );
 
